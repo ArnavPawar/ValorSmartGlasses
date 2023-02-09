14ed36be21b00eacebf157ce6bbbf047a983c5d1
Arnav Pawar <arnav.ap.pawar@gmail.com>
2023-02-09 02:39:08 -0500

    Worked on Connection to glasses and sending map display
    Map display and connection

--- SwifuiTest/Connections.swift
+++ SwifuiTest/Connections.swift
@@ -10,14 +10,14 @@ import ActiveLookSDK
 import SwiftUI
 
 struct Connections: View{
-    
-    @SwiftUI.State private var viewModel = GlassesTableViewController()
+    @ObservedObject var Glasses = GlassesTableViewController()
+    //@SwiftUI.State private var viewModel = GlassesTableViewController()
     
     var body: some View {//FIX THIS
         NavigationView{
             VStack{
                 Text("Scanning...")
-                Button("Scan", action: viewModel.runScan)
+                Button("Scan", action: Glasses.runScan)
             }//.navigationBarTitle("")
                // .navigationBarHidden(true)
         }
@@ -25,8 +25,9 @@ struct Connections: View{
 }
 
 class GlassesTableViewController: UITableViewController {
-    
-    
+    private let glassesName: String = "Proto 0144829"
+    private var glassesConnected: Glasses?    
+
     private let scanDuration: TimeInterval = 10.0
     private let connectionTimeoutDuration: TimeInterval = 5.0
 
@@ -37,7 +38,7 @@ class GlassesTableViewController: UITableViewController {
             guard let infoDictionary: [String: Any] = Bundle.main.infoDictionary else { return "" }
             guard let activelookSDKToken: String = infoDictionary["ACTIVELOOK_SDK_TOKEN"] as? String else { return "" }
             return activelookSDKToken
-        }()
+    }()
     
     private lazy var activeLook: ActiveLookSDK = {
             try! ActiveLookSDK.shared(
@@ -53,7 +54,7 @@ class GlassesTableViewController: UITableViewController {
                     }, onUpdateFailureCallback: { SdkGlassesUpdate in
                         print("onUpdateFailureCallback")
                     })
-        }()
+    }()
     
 //    activeLook.startScanning(
 //        onGlassesDiscovered: { [weak self] (discoveredGlasses: DiscoveredGlasses) in
@@ -64,137 +65,47 @@ class GlassesTableViewController: UITableViewController {
 //            print("error while scanning: \(error.localizedDescription)")
 //        }
 //    )
-    
-    private var discoveredGlassesArray: [DiscoveredGlasses] = []
-    private var connecting: Bool = false
-    
-    override func viewDidDisappear(_ animated: Bool) {
-            activeLook.stopScanning()
-            super.viewDidDisappear(animated)
-    }
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return discoveredGlassesArray.count
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "GlassesTableViewCell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = discoveredGlassesArray[indexPath.row].name
-        return cell
-    }
-        
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if connecting { return }
-        connecting = true
-
-        let selectedGlasses = discoveredGlassesArray[indexPath.row]
-
-        selectedGlasses.connect(
-            onGlassesConnected: { [weak self] (glasses: Glasses) in
-                guard let self = self else { return }
-                
-                self.connecting = false
-                self.connectionTimer?.invalidate()
-                if (glasses.isFirmwareAtLeast(version: "4.0")) {
-//                    (glasses.compareFirmwareAtLeast(version: "4.0").rawValue > 0) {
-//                        let alert = UIAlertController(title: "Update application", message: "The glasses firmware is newer. Check the store for an application update.", preferredStyle: .alert)
-//                        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-//                        self.present(alert, animated: true)
-//                         } else {
-                    
-                    // TO UPLOAD CONFIG, ADD YOUR CONFIG.TXT FILE TO THE PROJECT,
-                    // THEN UNCOMMENT THE FOLLOWING BLOCK /*...*/
-                    // AND FINALLY REPLACE THE RESSOURCE NAME WITH YOUR CONFIG'S NAME
-/*                         if let filePath = Bundle.main.path(forResource: "ConfigDemo-4.0.txt", ofType: "txt") {
-                             do {
-                                 let cfg = try String(contentsOfFile: filePath)
-                                 glasses.loadConfiguration(cfg: cfg.components(separatedBy: "\n"))
-                             } catch {}
-*/
-                    
-//                         }
-//                    }
-                    //let viewController = CommandsMenuTableViewController(glasses)
-                    //self.navigationController?.pushViewController(viewController, animated: true)
-                } else {
-                    let alert = UIAlertController(title: "Update glasses firmware", message: "The glasses firmware is not up to date.", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-                    self.present(alert, animated: true)
-                }
-            }, onGlassesDisconnected: { [weak self] in
-                guard let self = self else { return }
-                
-                let alert = UIAlertController(title: "Glasses disconnected", message: "Connection to glasses lost", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
-                    self.navigationController?.popToRootViewController(animated: true)
-                }))
-                
-                self.navigationController?.present(alert, animated: true)
-                
-            }, onConnectionError: { [weak self] (error: Error) in
-                guard let self = self else { return }
-                
-                self.connecting = false
-                self.connectionTimer?.invalidate()
-                
-                let alert = UIAlertController(title: "Error", message: "Connection to glasses failed: \(error.localizedDescription)", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self.present(alert, animated: true)
-            })
-
-        connectionTimer = Timer.scheduledTimer(withTimeInterval: connectionTimeoutDuration, repeats: false, block: { [weak self] (timer) in
-            guard let self = self else { return }
-
-            print("connection to glasses timed out")
-            self.connecting = false
-
-            let alert = UIAlertController(title: "Error", message: "The connection to the glasses timed out", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true)
-            
-            self.tableView.deselectRow(at: indexPath, animated: true)
-        })
-    }
-    
-    
-    // MARK: - Data
-    
-    private func addDiscoveredGlasses(_ glasses: DiscoveredGlasses) {
-        discoveredGlassesArray.append(glasses)
-        // TODO: // Use -performBatchUpdates:completion: instead of these methods, which will be deprecated in a future release. (introduced iOS 11)
-        tableView.beginUpdates()
-        tableView.insertRows(at: [IndexPath(row: self.discoveredGlassesArray.count - 1, section: 0)], with: .automatic)
-        tableView.endUpdates()
-    }
-    
-
-    // MARK: - Scan
-    
     private func startScanning() {
-        //scanNavigationItem.title = "Stop"
-
-        self.discoveredGlassesArray = []
-        self.tableView.reloadData()
-        
         activeLook.startScanning(
             onGlassesDiscovered: { [weak self] (discoveredGlasses: DiscoveredGlasses) in
-                self?.addDiscoveredGlasses(discoveredGlasses)
-
+                if discoveredGlasses.name == self!.glassesName{
+                    discoveredGlasses.connect(
+                        onGlassesConnected: { [weak self] (glasses: Glasses) in
+                            guard let self = self else { return }
+                            self.connectionTimer?.invalidate()
+                            self.stopScanning()
+                            self.glassesConnected = glasses
+                            self.glassesConnected?.clear()
+                        }, onGlassesDisconnected: { [weak self] in
+                            guard let self = self else { return }
+                            
+                            let alert = UIAlertController(title: "Glasses disconnected", message: "Connection to glasses lost", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+                                self.navigationController?.popToRootViewController(animated: true)
+                            }))
+                            
+                            self.navigationController?.present(alert, animated: true)
+                            
+                        }, onConnectionError: { [weak self] (error: Error) in
+                            guard let self = self else { return }
+                            self.connectionTimer?.invalidate()
+                            
+                            let alert = UIAlertController(title: "Error", message: "Connection to glasses failed: \(error.localizedDescription)", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self.present(alert, animated: true)
+                        })
+                }
             }, onScanError: { [weak self] (error: Error) in
                 self?.stopScanning()
             }
         )
-
+        
         scanTimer = Timer.scheduledTimer(withTimeInterval: scanDuration, repeats: false) { timer in
             self.stopScanning()
         }
     }
-    
+
+     
     private func stopScanning() {
         activeLook.stopScanning()
         //scanNavigationItem.title = "Scan"
@@ -204,5 +115,151 @@ class GlassesTableViewController: UITableViewController {
         activeLook.isScanning() ? stopScanning() : startScanning()
     }
     
+    private func generateImageFromMap() {
+        let mapSnapshotterOptions = MKMapSnapshotter.Options()
+        mapSnapshotterOptions.region = self.mapView.region
+        mapSnapshotterOptions.size = CGSize(width: 200, height: 200)
+        mapSnapshotterOptions.mapType = MKMapType.mutedStandard
+        mapSnapshotterOptions.showsBuildings = false
+        mapSnapshotterOptions.showsPointsOfInterest = false
+
+        let snapShotter = MKMapSnapshotter(options: mapSnapshotterOptions)
+        snapShotter.start() { snapshot, error in
+            if let image = snapshot?.image{
+                self.glassesConnected?.imgStream(image: image, x: 0, y: 0, imgStreamFmt: .MONO_4BPP_HEATSHRINK)
+            }else{
+                print("Missing snapshot")
+            }
+        }
     
+    }
+//     private var discoveredGlassesArray: [DiscoveredGlasses] = []
+//     private var connecting: Bool = false
+    
+//     override func viewDidDisappear(_ animated: Bool) {
+//             activeLook.stopScanning()
+//             super.viewDidDisappear(animated)
+//     }
+//     override func numberOfSections(in tableView: UITableView) -> Int {
+//         return 1
+//     }
+
+//     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//         return discoveredGlassesArray.count
+//     }
+
+    
+//     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//         let cell = tableView.dequeueReusableCell(withIdentifier: "GlassesTableViewCell", for: indexPath) as UITableViewCell
+//         cell.textLabel?.text = discoveredGlassesArray[indexPath.row].name
+//         return cell
+//     }
+        
+    
+//     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//         if connecting { return }
+//         connecting = true
+
+//         let selectedGlasses = discoveredGlassesArray[indexPath.row]
+
+//         selectedGlasses.connect(
+//             onGlassesConnected: { [weak self] (glasses: Glasses) in
+//                 guard let self = self else { return }
+                
+//                 self.connecting = false
+//                 self.connectionTimer?.invalidate()
+//                 if (glasses.isFirmwareAtLeast(version: "4.0")) {
+// //                    (glasses.compareFirmwareAtLeast(version: "4.0").rawValue > 0) {
+// //                        let alert = UIAlertController(title: "Update application", message: "The glasses firmware is newer. Check the store for an application update.", preferredStyle: .alert)
+// //                        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+// //                        self.present(alert, animated: true)
+// //                         } else {
+                    
+//                     // TO UPLOAD CONFIG, ADD YOUR CONFIG.TXT FILE TO THE PROJECT,
+//                     // THEN UNCOMMENT THE FOLLOWING BLOCK /*...*/
+//                     // AND FINALLY REPLACE THE RESSOURCE NAME WITH YOUR CONFIG'S NAME
+// /*                         if let filePath = Bundle.main.path(forResource: "ConfigDemo-4.0.txt", ofType: "txt") {
+//                              do {
+//                                  let cfg = try String(contentsOfFile: filePath)
+//                                  glasses.loadConfiguration(cfg: cfg.components(separatedBy: "\n"))
+//                              } catch {}
+// */
+                    
+// //                         }
+// //                    }
+//                     //let viewController = CommandsMenuTableViewController(glasses)
+//                     //self.navigationController?.pushViewController(viewController, animated: true)
+//                 } else {
+//                     let alert = UIAlertController(title: "Update glasses firmware", message: "The glasses firmware is not up to date.", preferredStyle: .alert)
+//                     alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//                     self.present(alert, animated: true)
+//                 }
+//             }, onGlassesDisconnected: { [weak self] in
+//                 guard let self = self else { return }
+                
+//                 let alert = UIAlertController(title: "Glasses disconnected", message: "Connection to glasses lost", preferredStyle: .alert)
+//                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+//                     self.navigationController?.popToRootViewController(animated: true)
+//                 }))
+                
+//                 self.navigationController?.present(alert, animated: true)
+                
+//             }, onConnectionError: { [weak self] (error: Error) in
+//                 guard let self = self else { return }
+                
+//                 self.connecting = false
+//                 self.connectionTimer?.invalidate()
+                
+//                 let alert = UIAlertController(title: "Error", message: "Connection to glasses failed: \(error.localizedDescription)", preferredStyle: .alert)
+//                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                 self.present(alert, animated: true)
+//             })
+
+//         connectionTimer = Timer.scheduledTimer(withTimeInterval: connectionTimeoutDuration, repeats: false, block: { [weak self] (timer) in
+//             guard let self = self else { return }
+
+//             print("connection to glasses timed out")
+//             self.connecting = false
+
+//             let alert = UIAlertController(title: "Error", message: "The connection to the glasses timed out", preferredStyle: .alert)
+//             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//             self.present(alert, animated: true)
+            
+//             self.tableView.deselectRow(at: indexPath, animated: true)
+//         })
+//     }
+    
+    
+//     // MARK: - Data
+    
+//     private func addDiscoveredGlasses(_ glasses: DiscoveredGlasses) {
+//         discoveredGlassesArray.append(glasses)
+//         // TODO: // Use -performBatchUpdates:completion: instead of these methods, which will be deprecated in a future release. (introduced iOS 11)
+//         tableView.beginUpdates()
+//         tableView.insertRows(at: [IndexPath(row: self.discoveredGlassesArray.count - 1, section: 0)], with: .automatic)
+//         tableView.endUpdates()
+//     }
+    
+
+//     // MARK: - Scan
+    
+//     private func startScanning() {
+//         //scanNavigationItem.title = "Stop"
+
+//         self.discoveredGlassesArray = []
+//         self.tableView.reloadData()
+        
+//         activeLook.startScanning(
+//             onGlassesDiscovered: { [weak self] (discoveredGlasses: DiscoveredGlasses) in
+//                 self?.addDiscoveredGlasses(discoveredGlasses)
+
+//             }, onScanError: { [weak self] (error: Error) in
+//                 self?.stopScanning()
+//             }
+//         )
+
+//         scanTimer = Timer.scheduledTimer(withTimeInterval: scanDuration, repeats: false) { timer in
+//             self.stopScanning()
+//         }
+//     }
 }
